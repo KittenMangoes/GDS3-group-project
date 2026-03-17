ef12e5c1c8339fbd21acf9521e32ac294abe2203
Sam <red.panda.sammy@gmail.com>
2026-03-17 13:05:42 +1100

    frog movement
    fixed the math, now the frog moves to the screen tap

--- Assets/scripts/findScreenClick.cs
+++ Assets/scripts/findScreenClick.cs
@@ -17,14 +17,13 @@ public class findScreenClick : MonoBehaviour
         {
             touch = Input.GetTouch(0);
             //Debug.Log(touch.position);
-            touchWorldCoords = Camera.main.ScreenToWorldPoint(new Vector2(touch.position.x,touch.position.y));//this as raw data shows (0,1,-10) despite being a vector2
-            Debug.Log(new Vector2(touchWorldCoords.x, touchWorldCoords.y));
-            //this is outputing (0,1) no matter where clicked, this shows that the issue just comes down to the math from the screen to world point function
+            touchWorldCoords = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x,touch.position.y,Camera.main.nearClipPlane + 10f));
+            Debug.Log(touchWorldCoords);
         }
     }
 
     public Vector3 GetClickPosition()
     {
-        return Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x,touch.position.y,0));
+        return new Vector2(touchWorldCoords.x, touchWorldCoords.y);
     }
 }
--- Assets/scripts/moveToClick.cs
+++ Assets/scripts/moveToClick.cs
@@ -14,10 +14,12 @@ public class moveToClick : MonoBehaviour
     void Update()
     {
         //Debug.Log(findScreenClick.findScreenClickInstance.GetClickPosition());
-        Vector3 target = findScreenClick.findScreenClickInstance.GetClickPosition();
+        Vector2 target = findScreenClick.findScreenClickInstance.GetClickPosition();
         if (thisTransform.position.x != target.x && thisTransform.position.y != target.y);
         {
-            thisTransform.position += new Vector3(target.x, target.y, 0) * Time.deltaTime * speed;
+            thisTransform.position += new Vector3((target.x - thisTransform.position.x), (target.y - thisTransform.position.y), 0).normalized * Time.deltaTime * speed;
+            //thisTransform.LookAt(target);
+            //thisTransform.position += this.thisTransform.forward * Time.deltaTime * speed;
         }
     }
 }
