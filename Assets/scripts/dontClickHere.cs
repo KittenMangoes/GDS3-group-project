0f9bfd0e4f7663ee869f5a7579078123418fdd8d
KittenMangoes <red.panda.sammy@gmail.com>
2026-05-09 19:57:09 +1000

    water shader
    added an animated water background using a shader graph

--- Assets/scripts/dontClickHere.cs
+++ Assets/scripts/dontClickHere.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class dontClickHere : MonoBehaviour
 {
-    [SerializeField] private float maxCanvasX;
+    [SerializeField] private float maxCanvasX;//the canvas is an editable size in the editor
     [SerializeField] private float maxCanvasY;
     [SerializeField] private float minCanvasX;
     [SerializeField] private float minCanvasY;
@@ -10,15 +10,15 @@ public class dontClickHere : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 touch = findScreenClick.findScreenClickInstance.GetClickPosition();
+        Vector3 touch = findScreenClick.findScreenClickInstance.GetClickPosition();//if you click outside of the canvas
         if (touch.x > maxCanvasX ||
             touch.x < minCanvasX ||
             touch.y > maxCanvasY ||
             touch.y < minCanvasY)
-        {//if you click outside of the canvas
+        {//set the touch position to the player so it doesnt move
             Vector3 playerPos = findScreenClick.findScreenClickInstance.GetPlayerPosition();
-            //findScreenClick.findScreenClickInstance.ChangeTouchPosition(playerPos);
+            //findScreenClick.findScreenClickInstance.ChangeTouchPosition(playerPos); this doesn't work because it takes the players world coords and treats them like screen coords
             findScreenClick.findScreenClickInstance.ChangeTouchPosition(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width/2, Screen.height/2, Camera.main.nearClipPlane + 10f)));
-        }//set the touch position to the player so it doesnt move
+        }
     }
 }
